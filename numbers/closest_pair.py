ad13754ad4fa6fa5be5b3f778421c21f4b68f2e7
dennis <dennis@dennischaves.xyz>
2021-12-23 21:07:36 -0500

    worked on closest pair kata

--- numbers/closest_pair.py
+++ numbers/closest_pair.py
@@ -16,6 +16,19 @@ def create_coordinates(xmax, ymax, n):
 
     return coords
 
+def unique_list(l):
+    """Removes duplicate elements from a list.
+    list -> unique list."""
+    ul = []
+    for i in l:
+        if i not in ul:
+            ul.append(i)
+    return ul
+
+POINTS = create_coordinates(123, 456, 100)
+POINTS = unique_list(POINTS)
+print(POINTS)
+
 #POINTS = [
     #(82, 65), (2, 64), (43, 39), (34, 17), (8, 39), (96, 28), (64, 78), (76, 2),
     #(99, 91), (14, 25), (94, 64), (58, 66), (17, 42), (93, 41), (22, 50), (7, 10),
@@ -31,24 +44,26 @@ def create_coordinates(xmax, ymax, n):
     #(55, 71), (71, 11), (40, 3), (40, 81), (66, 3), (62, 62), (62, 95), (47, 79),
     #(66, 18), (43, 78), (66, 5), (31, 41)
 #]
-POINTS = [ (82, 65), (2, 64), (43, 39), (34, 17), (8, 39)]
+#POINTS = [ (82, 65), (2, 64), (43, 39), (34, 17), (8, 39)]
 
 # Distance between two points:
 # d = sqrt( (x1 - x2)^2 + (y1 - y2)^2 )
 
-smallest_distance = sqrt( (0 - 100)**2 + (100 - 0)**2 )
+smallest_distance = ( (0, 100), (100, 0), sqrt( (0 - 100)**2 + (100 - 0)**2 ) )
 print(smallest_distance)
 
 for point1 in POINTS:
     temp_list = POINTS.copy()
     temp_list.remove(point1)
-    print(temp_list)
+    #print(temp_list)
     for point2 in temp_list:
-        print(point1, point2)
+        #print(point1, point2)
         x1 = point1[0]
         x2 = point2[0]
         y1 = point1[1]
         y2 = point2[1]
         distance = sqrt( (x1 - x2)**2 + (y1 - y2)**2 )
-        if distance < smallest_distance:
-            smallest_distance = distance
+        if distance < smallest_distance[2]:
+            smallest_distance = ( (x1, y1), (x2, y2), distance)
+
+print(smallest_distance)
