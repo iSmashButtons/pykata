61c820ba4921a40fd9c5afa3fd7f20ee873d41d4
Dennis Chaves <dennis@dennischaves.xyz>
2021-12-27 20:53:55 -0500

    finished reverse string, worked on piglatin

--- text/reversestring.py
+++ text/reversestring.py
@@ -1,2 +1,17 @@
 # **Reverse a String** - Enter a string and the program will reverse it and
 # print it out.
+
+def reverse_string(instr):
+    inlist = list(instr)
+    inlist.reverse()
+    reverse_string = ''
+    for i, v in enumerate(inlist):
+        reverse_string += inlist[i]
+    return reverse_string
+
+while True:
+    print("Enter a sting and I'll reverse it!\nEnter 'q' to quit.")
+    instring = input(">>> ")
+    if 'q' in instring:
+        break
+    print('\n' + reverse_string(instring) + '\n')
