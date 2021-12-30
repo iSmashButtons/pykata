e620f905e81dedc13d433281ba0a92d87ddf36b5
Dennis Chaves <Dennis.Chaves@chesterton.com>
2021-12-30 09:28:17 -0500

    worked on countVowels.py

--- text/countVowels.py
+++ text/countVowels.py
@@ -2,11 +2,12 @@
 # and the program counts the number of vowels in the text. For added complexity
 # have it report a sum of each vowel found.
 
+VOWELS = ['a','e','i','o','u']
+
 def total_vowels(s):
     """ 
     Counts the vowels within a string and returns the total.
     """ 
-    VOWELS = ['a','e','i','o','u']
     words =  s.lower().split(' ')
     num_vowels = 0
     for word in words:
@@ -18,10 +19,25 @@ def total_vowels(s):
 
 # TODO: finish this function
 def sum_each_vowel(s):
-    pass
+    """Counts the vowels witin a string and returns a dictionary with the sum
+    total of each vowl.
+    """   
+    words =  s.lower().split(' ')
+    sv = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+    for word in words:
+        for l in word:
+            if l in VOWELS:
+                sv[l] += 1
+    return sv
 
 while True:
     print("Enter a string and I will tell you the total number of vowels ('q' to quit').")
-    instring = input('>>> ')
-    print(total_vowels(instring))
+    #instring = input('>>> ')
+    instring = "I fart in your general direction!"
+    print('Total vowels: ', total_vowels(instring))
+    vowel_totals_gen = (i for i in sum_each_vowel(instring).values())
+    for i in vowel_totals_gen:
+        print(type(i), i)
+    #sum_vowels = sum(vowel_totals_gen)
+    print('Sum of each vowel: ', sum_each_vowel(instring))
     break
