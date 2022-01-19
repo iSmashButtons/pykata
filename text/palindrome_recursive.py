8501d4f43d114f89d1d04586b1289a4116383e65
Dennis Chaves <Dennis.Chaves@chesterton.com>
2022-01-19 09:15:47 -0500

    renamed some files

---
+++ text/palindrome_recursive.py
@@ -0,0 +1,17 @@
+# Check if Palindrome: Checks if the string entered by the user is a
+# palindrome. That is that it reads the same forwards as backwards like “racecar”
+
+def is_palindrome(inword):
+    """
+    Checks to see if `inword` is a palindrome (reversing the order of the letters
+    produces the same word.) Returns True or False
+    """
+    pass
+
+teststr1 = 'racecar'
+teststr2 = 'recursive'
+teststr3 = 'kayak'
+
+print(is_palindrome(teststr1))
+print(is_palindrome(teststr2))
+print(is_palindrome(teststr3))
